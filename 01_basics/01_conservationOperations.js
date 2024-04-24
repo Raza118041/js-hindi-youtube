9e37f323dda7e97ca664b007ae39f4990cfca95c
Ali Raza <aliraza118041@gmail.com>
2024-04-24 23:53:50 +0500

    conversions of operations

---
+++ 01_basics/01_conservationOperations.js
@@ -0,0 +1,19 @@
+// let score = string
+
+
+// let value = Number(score)
+// console.log(value);
+
+
+let boolean = 0
+let booleanNumber = Boolean(boolean)
+console.log(booleanNumber);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
