4229b39f83066e24b539cc81911928d13291b50d
Ali Raza <aliraza118041@gmail.com>
2024-04-25 00:51:15 +0500

    operations and dataTypes

--- 01_basics/03_operations.js
+++ 01_basics/03_operations.js
@@ -5,6 +5,13 @@
 // console.log(2**2);
 // console.log(+true);
 
-let a = 6
-a++
- console.log(a);
+// let a = 6
+// a++
+//  console.log(a);
+
+// Comparison Operators
+
+//  console.log(10 < 0);
+//  console.log(10 != 10) // Not equal to Operator
+//  console.log(10 != 12)
+console.log(3 !== 3)
---
+++ 01_basics/04_dataTypes.js
@@ -0,0 +1,8 @@
+const Number1 = Symbol('123')
+const Number2 = Symbol('123')
+
+console.log(Number1 !== Number2);
+
+const bigNumber = 1235
+
+console.log(typeof bigNumber);
